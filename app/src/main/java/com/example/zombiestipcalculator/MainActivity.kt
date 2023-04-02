a535c59fb8ef75ba283b7b429461a79183d3c651
Zombie <digitalwormm@gmail.com>
2023-04-02 19:12:00 +0800

    Finished Application

--- app/src/main/java/com/example/zombiestipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/zombiestipcalculator/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.zombiestipcalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.zombiestipcalculator.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateBTN.setOnClickListener{ calculateTip() }
     }
+
+    private fun calculateTip(){
+        val str = binding.costOfService.text.toString()
+        val cst = str.toDoubleOrNull()
+        if (cst == null){
+            binding.tipResult.text = ""
+            binding.costOfService.text.clear()
+            Toast.makeText(this, "Please enter a number", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val selectedTip = binding.tipOptions.checkedRadioButtonId
+        val roundUp = binding.roundUpSwitch.isChecked
+
+        // Tip Percentage
+        val res = when (selectedTip){
+            R.id.optionTwentyPercent   -> 0.20
+            R.id.optionEighteenPercent -> 0.18
+            else                       -> 0.15
+        }
+
+        // TIP = RES * CST
+        var tip = if (roundUp) res * cst else kotlin.math.ceil(res*cst)
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_Result, formattedTip)
+    }
+
+
 }
